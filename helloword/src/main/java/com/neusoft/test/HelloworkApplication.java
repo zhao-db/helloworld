242d37d7d60bfcc52e443eef0f75b44eef83a145
zhao-db <zhao-db@neusoft.com>
2019-02-20 15:27:55 +0800

    发布项目

---
+++ helloword/src/main/java/com/neusoft/test/HelloworkApplication.java
@@ -0,0 +1,22 @@
+package com.neusoft.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworkApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
+	}
+
+}
